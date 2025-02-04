f624cc9bc62386b73eb68d8551cd0d8a91664a19
Emiliano Gonzalez <EMILIANO_G06@hotmail.com>
2025-02-04 16:37:07 -0300

    Add project files.

---
+++ WebAppProducts/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppProducts.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(150)]
+    public string? Description { get; set; }
+    [Range(0, int.MaxValue, ErrorMessage = "Stock cannot be negative")]
+    public int Stock { get; set; }
+}
