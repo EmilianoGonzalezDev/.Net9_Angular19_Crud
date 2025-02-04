ab923cfa0d90e6d248a94c0f9b361437b7f0d16d
Emiliano Gonzalez <EMILIANO_G06@hotmail.com>
2025-02-04 17:05:11 -0300

    include Angular frontend in solution

---
+++ Frontend/.angular/cache/19.1.5/front-productos/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-CITVFPT4.js";
+import "./chunk-5BFLJX7B.js";
+import "./chunk-FCZDIC76.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ Frontend/src/app/products/components/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [MatButtonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+}
